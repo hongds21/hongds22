5615227c62def82be5c623fac4de7f706ce8208d
hongds21 <21158099@student.hcmute.edu.vn>
2025-05-02 11:44:35 +0700

    Add files via upload

---
+++ GIỚI THIỆU TÊN.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import colour
+import numpy as np
+
+st.set_page_config(page_title="Khoa Hoc Mau Sac Streamlit App", layout="wide", page_icon="👋")
+st.write("### Chào mừng bạn đến với Project Khoa học Màu sắc của mình! 👋")
+st.write("## Mình tên là DƯƠNG SƠN HỒNG , MaSV: 21158099")
+
+st.sidebar.success("Bạn hãy chọn các mục ở trên")
